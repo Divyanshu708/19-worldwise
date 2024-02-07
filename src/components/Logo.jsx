53057efbc4d04c2021a1d0c09fa8aa1dc0d65358
divyanshu708 <divyanshumj756@gmail.com>
2024-02-07 15:44:53 +0530

    Finished - 211. Building the App Layout

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 import styles from "./Logo.module.css";
-
+import logo from "../assets/logo.png";
 function Logo() {
-  return <img src="" alt="WorldWise logo" className={styles.logo} />;
+  return <img src={logo} alt="WorldWise logo" className={styles.logo} />;
 }
 
 export default Logo;
