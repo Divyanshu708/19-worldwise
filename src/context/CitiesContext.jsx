9841ae9178dfd01dd0a5c59e10d4ff84ccf5f0b5
Divyanshu MJ <141554205+Divyanshu708@users.noreply.github.com>
2024-02-17 16:54:37 +0530

    Finished - 227. Back to WorldWise: Creating a CitiesContext

--- src/context/CitiesContext.jsx
+++ src/context/CitiesContext.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 
-import { createContext, useState, useEffect } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 const CitiesContext = createContext();
 const BASE_URL = "http://localhost:9000";
@@ -32,4 +32,10 @@ function CitiesProvider({ children }) {
   );
 }
 
-export { CitiesProvider };
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CitiesContext was used outside the CitiesProvider");
+  return context;
+}
+export { CitiesProvider, useCities };
